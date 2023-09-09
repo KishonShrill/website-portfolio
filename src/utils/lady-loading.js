c07ac57adcc5c878a616ec806142327ab680a858
KishonShrill <crystalbluew@gmail.com>
2023-09-09 16:27:39 +0800

    first commit

---
+++ src/utils/lady-loading.js
@@ -0,0 +1,31 @@
+const lazyLoading = () => {
+  const lazyImgs = document.querySelectorAll('.lazy');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    // console.log(entries);
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let img = entry.target;
+        img.src = img.dataset.src;
+        img.classList.remove('loading');
+        img.classList.add('loaded');
+        observer.unobserve(img);
+      }
+    });
+  });
+
+  lazyImgs.forEach(img => {
+    observer.observe(img);
+  });
+};
+
+export default lazyLoading;
+
+//! Inefficient
+/* 
+const observer = new IntersectionObserver(() => {}, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0,
+});
+*/
