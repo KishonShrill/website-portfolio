a6690236e48773d1b2d13cf8b326b8ba6c62b817
KishonShrill <crystalbluew@gmail.com>
2026-05-08 22:43:08 +0800

    chore: changing component scripts from cjs to mjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { scan } from 'react-scan'
-import { useState, useEffect, lazy } from 'react';
+import { useEffect, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from 'react-query';
 
 // --- React Query Setup ---
 const queryClient = new QueryClient();
-scan({enabled: import.meta.env.VITE_SCAN === "true",});
+scan({ enabled: import.meta.env.VITE_SCAN === "true", });
 
 // --- Component Lazy Loading ---
 // Use React.lazy to load page components only when needed
 const Homepage = lazy(() => import('./pages/Homepage.jsx'));
-const Database_List = lazy(() => import('./pages/Database_List.jsx'));
 const NoPage = lazy(() => import('./pages/NoPage.jsx'));
-const SpecificStudent = lazy(() => import('./components/SpecificStudent.jsx'));
 
 // Project Pages (Lazy Loaded)
 const AgeCalculatorApp = lazy(() => import('./pages/projects/AgeCalculatorApp.jsx'));
@@ -33,76 +31,76 @@ const RockPaperScissor = lazy(() => import('./pages/projects/RockPaperScissor.js
 
 
 export default function App() {
-  const body = document.body
-  const theme = localStorage.getItem('theme');
-  theme && body.classList.add(theme);
+    const body = document.body
+    const theme = localStorage.getItem('theme');
+    theme && body.classList.add(theme);
 
 
-  // Theme getter for local user setting
-  function themeToggle() {
-    let currentTheme = localStorage.getItem('theme');
-    console.log("Am I running")
+    // Theme getter for local user setting
+    function themeToggle() {
+        let currentTheme = localStorage.getItem('theme');
+        console.log("Am I running")
 
-    if (currentTheme === 'dark-mode') {
-      localStorage.setItem('theme', 'light-mode');
-      body.classList.remove('dark-mode');
-      body.classList.add('light-mode');
-    } else {
-      localStorage.setItem('theme', 'dark-mode');
-      body.classList.remove('light-mode');
-      body.classList.add('dark-mode');
-    }
-  }
-  
-  // Intersection Observers for Lazy Classes
-  useEffect(() => {
-    const lazyImgs = document.querySelectorAll('.lazy');
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          let img = entry.target;
-          img.src = img.dataset.src;
-          img.classList.remove('loading');
-          img.classList.add('loaded');
-          observer.unobserve(img);
+        if (currentTheme === 'dark-mode') {
+            localStorage.setItem('theme', 'light-mode');
+            body.classList.remove('dark-mode');
+            body.classList.add('light-mode');
+        } else {
+            localStorage.setItem('theme', 'dark-mode');
+            body.classList.remove('light-mode');
+            body.classList.add('dark-mode');
         }
-      });
-    });
-    lazyImgs.forEach(img => {observer.observe(img);});
-    return () => {
-      observer.disconnect(); // Cleanup when component unmounts
-    };
-  }, []);
+    }
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Homepage themeToggle={themeToggle} />} exact />
-            <Route path="*" element={<NoPage />} />
+    // Intersection Observers for Lazy Classes
+    useEffect(() => {
+        const lazyImgs = document.querySelectorAll('.lazy');
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    let img = entry.target;
+                    img.src = img.dataset.src;
+                    img.classList.remove('loading');
+                    img.classList.add('loaded');
+                    observer.unobserve(img);
+                }
+            });
+        });
+        lazyImgs.forEach(img => { observer.observe(img); });
+        return () => {
+            observer.disconnect(); // Cleanup when component unmounts
+        };
+    }, []);
 
-
-            <Route path="/projects/age_calculator_app" element={<AgeCalculatorApp title="Frontend Mentor | Age Calculator App" />} />
-            <Route path="/projects/qr_code_component" element={<QrCodeComponent title="Frontend Mentor | QR Code Component" />} />
-            <Route path="/projects/news_homepage" element={<NewsHomepage title="Frontend Mentor | News Homepage" />} />
-            <Route path="/projects/product_preview_card_component" element={<ProductPreviewCard title="Frontend Mentor | Product Preview Card" />} />
-            <Route path="/projects/nft_preview_card_component" element={<NFTPreviewCard title="Frontend Mentor | NFT Preview Card Component" />} />
-            <Route path="/projects/four_card_feature_component" element={<FourCardFeature title="Frontend Mentor | Four Card Feature Component" />} />
-            <Route path="/projects/profile_card_component" element={<ProfileCardComponent title="Frontend Mentor | Profile Card Component" />} />
-            <Route path="/projects/results_summary_component" element={<ResultSummaryComponent title="Frontend Mentor | Results Summary Component" />} />
-            <Route path="/projects/interactive_card_details_form" element={<InteractiveCardDetailsForm title="Frontend Mentor | Interactive Card Details Form Component" />} />
-            <Route path="/projects/interactive_rating_component" element={<InteractiveRatingComponent title="Frontend Mentor | Interactive Rating Component" />} />
-            <Route path="/projects/newsletter_sign_up_with_success_message" element={<NewsletterSignUpProject title="Frontend Mentor | Newsletter Sign Up" />} />
-            <Route path="/projects/newsletter_sign_up_with_success_message/success" element={<NewsletterSignUpProjectSuccess title="Frontend Mentor | Newsletter Sign Up Success" />} />
-            <Route path="/projects/calculator_app" element={<CalculatorApp title="Frontend Mentor | CalculatorApp" />} />
-            <Route path="/projects/blog_preview_card" element={<BlogPreviewCard title="Frontend Mentor | Blog Preview Card" />} />
-            <Route path="/projects/rock_paper_scissor" element={<RockPaperScissor title="Frontend Mentor | Rock Paper Scissor" />} />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Homepage themeToggle={themeToggle} />} exact />
+                    <Route path="*" element={<NoPage />} />
 
 
-            <Route path="/database" element={<Database_List />} />
-            <Route path="/database/:studentId" element={<SpecificStudent />} />
-          </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  );
+                    <Route path="/projects/age_calculator_app" element={<AgeCalculatorApp title="Frontend Mentor | Age Calculator App" />} />
+                    <Route path="/projects/qr_code_component" element={<QrCodeComponent title="Frontend Mentor | QR Code Component" />} />
+                    <Route path="/projects/news_homepage" element={<NewsHomepage title="Frontend Mentor | News Homepage" />} />
+                    <Route path="/projects/product_preview_card_component" element={<ProductPreviewCard title="Frontend Mentor | Product Preview Card" />} />
+                    <Route path="/projects/nft_preview_card_component" element={<NFTPreviewCard title="Frontend Mentor | NFT Preview Card Component" />} />
+                    <Route path="/projects/four_card_feature_component" element={<FourCardFeature title="Frontend Mentor | Four Card Feature Component" />} />
+                    <Route path="/projects/profile_card_component" element={<ProfileCardComponent title="Frontend Mentor | Profile Card Component" />} />
+                    <Route path="/projects/results_summary_component" element={<ResultSummaryComponent title="Frontend Mentor | Results Summary Component" />} />
+                    <Route path="/projects/interactive_card_details_form" element={<InteractiveCardDetailsForm title="Frontend Mentor | Interactive Card Details Form Component" />} />
+                    <Route path="/projects/interactive_rating_component" element={<InteractiveRatingComponent title="Frontend Mentor | Interactive Rating Component" />} />
+                    <Route path="/projects/newsletter_sign_up_with_success_message" element={<NewsletterSignUpProject title="Frontend Mentor | Newsletter Sign Up" />} />
+                    <Route path="/projects/newsletter_sign_up_with_success_message/success" element={<NewsletterSignUpProjectSuccess title="Frontend Mentor | Newsletter Sign Up Success" />} />
+                    <Route path="/projects/calculator_app" element={<CalculatorApp title="Frontend Mentor | CalculatorApp" />} />
+                    <Route path="/projects/blog_preview_card" element={<BlogPreviewCard title="Frontend Mentor | Blog Preview Card" />} />
+                    <Route path="/projects/rock_paper_scissor" element={<RockPaperScissor title="Frontend Mentor | Rock Paper Scissor" />} />
+
+
+                    <Route path="/database" element={<Database_List />} />
+                    <Route path="/database/:studentId" element={<SpecificStudent />} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
+    );
 }
--- src/pages/projects/AgeCalculatorApp.jsx
+++ src/pages/projects/AgeCalculatorApp.jsx
@@ -1,65 +1,65 @@
 import { useEffect } from "react"
-import ageCalculator from "./components/ageCalculator.cjs"
+import ageCalculator from "./components/ageCalculator.mjs"
 import('./styles/age-calculator.css')
 
 const AgeCalculatorApp = (props) => {
-  document.title = props.title
-  useEffect(() => {
-    ageCalculator();
-  }, [])
+    document.title = props.title
+    useEffect(() => {
+        ageCalculator();
+    }, [])
 
-  return(
-    <main className="age-calculator">
-      <main className="age-processor">
-        <section className="date__inputs-container">
-          <div><h2 className="date__legend">D A Y</h2>
-            <input className="date__input input__day" type="number" min="1" max="2" placeholder="DD" />
-            <p className="date__input-error"><em className="error-day"> </em></p>
-          </div>
-          <div><h2 className="date__legend">M O N T H</h2>
-            <input className="date__input input__month" type="number" min="1" max="2" placeholder="MM" />
-            <p className="date__input-error"><em className="error-month"> </em></p>
-          </div>
-          <div><h2 className="date__legend">Y E A R</h2>
-            <input className="date__input input__year" type="number" min="1" max="4" placeholder="YYYY" />
-            <p className="date__input-error"><em className="error-year"> </em></p>
-          </div>
-        </section>
-        <hr />
-        <button className="checker__btn">
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            width="46" 
-            height="44" 
-            viewBox="0 0 46 44"
-          >
-            <g 
-              fill="none" 
-              stroke="#FFF" 
-              strokeWidth="2"
-            >
-              <path 
-                d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 
+    return (
+        <main className="age-calculator">
+            <main className="age-processor">
+                <section className="date__inputs-container">
+                    <div><h2 className="date__legend">D A Y</h2>
+                        <input className="date__input input__day" type="number" min="1" max="2" placeholder="DD" />
+                        <p className="date__input-error"><em className="error-day"> </em></p>
+                    </div>
+                    <div><h2 className="date__legend">M O N T H</h2>
+                        <input className="date__input input__month" type="number" min="1" max="2" placeholder="MM" />
+                        <p className="date__input-error"><em className="error-month"> </em></p>
+                    </div>
+                    <div><h2 className="date__legend">Y E A R</h2>
+                        <input className="date__input input__year" type="number" min="1" max="4" placeholder="YYYY" />
+                        <p className="date__input-error"><em className="error-year"> </em></p>
+                    </div>
+                </section>
+                <hr />
+                <button className="checker__btn">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="46"
+                        height="44"
+                        viewBox="0 0 46 44"
+                    >
+                        <g
+                            fill="none"
+                            stroke="#FFF"
+                            strokeWidth="2"
+                        >
+                            <path
+                                d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 
                 22.019C37.667 21.686 23 25.616 23 44"/>
-            </g>
-          </svg>
-        </button>
-        <section className="date__output-container">
-          <h1 className="date__output"><i><span className="date__answer" id="years">- -</span> years</i></h1>
-          <h1 className="date__output"><i><span className="date__answer" id="months">- -</span> months</i></h1>
-          <h1 className="date__output"><i><span className="date__answer" id="days">- -</span> days</i></h1>
-        </section>
-      </main>
-      
-      <footer className="attribution">
-        <div>
-          <p className="web">Challenge by <a className="mentor" href="https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q/hub" target="_blank"> Frontend Mentor</a>. 
-          Coded by <a href="https://github.com/KishonShrill"> Chriscent Pingol</a>.</p>
-        </div>
+                        </g>
+                    </svg>
+                </button>
+                <section className="date__output-container">
+                    <h1 className="date__output"><i><span className="date__answer" id="years">- -</span> years</i></h1>
+                    <h1 className="date__output"><i><span className="date__answer" id="months">- -</span> months</i></h1>
+                    <h1 className="date__output"><i><span className="date__answer" id="days">- -</span> days</i></h1>
+                </section>
+            </main>
 
-        <p className="phone">Go back <a href="../../"> Home</a>.</p>
-      </footer>
-    </main>
-  )
+            <footer className="attribution">
+                <div>
+                    <p className="web">Challenge by <a className="mentor" href="https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q/hub" target="_blank"> Frontend Mentor</a>.
+                        Coded by <a href="https://github.com/KishonShrill"> Chriscent Pingol</a>.</p>
+                </div>
+
+                <p className="phone">Go back <a href="../../"> Home</a>.</p>
+            </footer>
+        </main>
+    )
 }
 export default AgeCalculatorApp
